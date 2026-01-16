bc355239311d1a111baa05d1b0789e7a350f0b57
Nsovo <mnsovo2@gmail.com>
2026-01-16 10:46:12 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+document.querySelectorAll(".fade-in").forEach((el) => {
+  observer.observe(el);
+});
